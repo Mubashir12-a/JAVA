89c7133def31104222857364d21314254d896cb1
mubashir12-a <ma2625645@gmail.com>
2024-12-05 19:22:16 +0530

    05-12-2024

--- Projects/Project_02/Main.java
+++ Projects/Project_02/Main.java
@@ -9,23 +9,21 @@ public class Main {
         Add_Account_Holder add_Account_Holder = new Add_Account_Holder();
         File_Handling file_Handling = new File_Handling();
 
-        // file_Handling.Erase_All_Data();
+        // file_Handling.Erase_All_Data();        // Clears All Data Inside File.
 
-        // add_Account_Holder.Take_No_Of_Entries(scanner);
-        // Data [] data = new Data[Data.Entries];
+        // add_Account_Holder.Take_No_Of_Entries(scanner);   // Takes Number Of enteries to let Loop Go on Without An Error.
+        // Data [] data = new Data[Data.Entries];     // creates Data Class Array.
 
-        // file_Handling.ReadLast_Ac_No();
-        // //System.out.println(Data.base_Ac_no);
-        // file_Handling.Read_Serial_Number();
-        // //System.out.println(Data.Serial_No);
+        // file_Handling.ReadLast_Ac_No();     // It reads Last Account number aloted to Holder;
+        // //System.out.println(Data.base_Ac_no); // For check;
+        // file_Handling.Read_Serial_Number();  // It reads last Serial number aloted to Holder;
+        // //System.out.println(Data.Serial_No);    // For check;
             
-        // add_Account_Holder.Take_Input(data, scanner, file_Handling);
-        // file_Handling.Write_On_File(data);
+        // add_Account_Holder.Take_Input(data, scanner, file_Handling); // It takes Input of Data for Account Creation;
+        // file_Handling.Write_On_File(data);   // It writes Data on file for storage;
 
-        // file_Handling.Get_Total_Amount_InBAnk();
+        // file_Handling.Get_Total_Amount_InBAnk(); // It checks the total amount in bank;
 
-
-        
         scanner.close();
     }
 }
