77c4104d9decee5964885926b0b729e83710472e
mubashir12-a <ma2625645@gmail.com>
2025-01-12 17:07:06 +0530

    12-01-2025

--- JAVA_Topics/Interfaces.java
+++ JAVA_Topics/Interfaces.java
@@ -6,6 +6,12 @@ public class Interfaces {
         Model_11R.Flash();
         Model_11R.Camera();
         Model_11R.Speaker();
+
+        SmartWatch Watch_1 = new SmartWatch();
+
+        Watch_1.Flash();
+        Watch_1.RAM();
+        // So-on;
     }
 }
 
@@ -13,6 +19,13 @@ interface Mobile_Functions {
     public void Flash();
     public void Camera();
     public void Speaker();
+}
+
+interface HardWare {
+    public void Battery();
+    public void RAM();
+    public void ROM();
+    public void Display();
 }
 
 class Oneplus implements Mobile_Functions {
@@ -26,5 +39,36 @@ class Oneplus implements Mobile_Functions {
 
     public void Speaker(){
         System.out.println("Oneplus got Speakers");
+    }
+}
+
+
+class SmartWatch implements Mobile_Functions, HardWare {
+    public void Flash(){
+        System.out.println("Watch Has flash-light");
+    }
+
+    public void Camera(){
+        System.out.println("Watch Has Camera");
+    }
+
+    public void Speaker(){
+        System.out.println("Watch Has Speaker");
+    }
+
+    public void Battery(){
+        System.out.println("Watch Has Battery");
+    }
+
+    public void RAM(){
+        System.out.println("Watch Has RAM");
+    }
+
+    public void ROM(){
+        System.out.println("Watch Has ROM");
+    }
+
+    public void Display(){
+        System.out.println("Watch Has Display");
     }
 }
