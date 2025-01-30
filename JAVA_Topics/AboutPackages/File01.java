dc3f78e2942ff7616c1a937d205ffccc3fa3f2b1
mubashir12-a <ma2625645@gmail.com>
2025-01-30 19:00:51 +0530

    30-01-2025

---
+++ JAVA_Topics/AboutPackages/File01.java
@@ -0,0 +1,11 @@
+import JAVA_Topics.AboutPackages.*;
+
+public class File01 {
+    public static void main(String[] args) {
+        System.out.println("Testing...");
+
+        Math_Actions math = new Math_Actions();
+
+        System.out.println(math.Add(10, 20));
+    }
+}
---
+++ JAVA_Topics/AboutPackages/Pack01.java
@@ -0,0 +1,19 @@
+package JAVA_Topics.AboutPackages;
+
+class Math_Actions {
+    public int Add(int a, int b){
+        return a + b;
+    }
+
+    public int Sub(int a, int b){
+        return a - b;
+    }
+
+    public int Mul(int a, int b){
+        return a * b;
+    }
+
+    public int Div(int a, int b){
+        return a / b;
+    }
+}
